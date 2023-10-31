ffacca995dc21c101639a9fc256ecac3ffaa4d6a
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2023-10-31 18:14:41 +0300

    0

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+/*
+public class Main {
+    public static void main(String[] args) {
+        University uni1 = new University("1", "Университет Дантистов Номер Один", "УДНО", 2023, StudyProfile.DENTIST);
+        Student stu1 = new Student("Дантистов Дантисте Дантистович", "1", 1, 5);
+        System.out.print(uni1);
+        System.out.print(stu1);
+    }
+}*/
---
+++ src/main/java/org/example/StudyProfile.java
@@ -0,0 +1,11 @@
+package org.example;
+/*
+public enum StudyProfile {
+    DENTIST("Дантист"), SURGEON("Хирург"), PSYCHIATRIST("Психиатр");
+
+    private String profileName;
+
+    StudyProfile(String profileName) {
+        this.profileName = profileName;
+    }
+}*/
