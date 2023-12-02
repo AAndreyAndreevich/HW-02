bc32bfa9c045c2dbdfdc92a5c86ed862a21b9f54
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2023-12-02 05:10:57 +0300

    0.89

--- src/main/java/org/example/utils/XLSWriter.java
+++ src/main/java/org/example/utils/XLSWriter.java
@@ -14,30 +14,32 @@ public class XLSWriter {
     public static void statisticsWriter(List<Statistics> statisticsList, String filePath) throws IOException {
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("Статистика");
-        CellStyle style = workbook.createCellStyle();
+        CellStyle headStyle = workbook.createCellStyle();
         Font font = workbook.createFont();
         font.setBold(true);
-        font.setFontHeight((short)12);
-        style.setFont(font);
+        font.setFontHeight((short)11);
+        font.setFontName("Arial");
+        headStyle.setFont(font);
         int rowNum = 0;
-        Row row = sheet.createRow(rowNum);
-        row.createCell(0).setCellValue("Профиль обучения");
-        row.getCell(0).setCellStyle(style);
-        row.createCell(1).setCellValue("Средний балл");
-        row.getCell(1).setCellStyle(style);
-        row.createCell(2).setCellValue("Количество студентов");
-        row.getCell(2).setCellStyle(style);
-        row.createCell(3).setCellValue("Количество университетов");
-        row.getCell(3).setCellStyle(style);
-        row.createCell(4).setCellValue("Университеты");
-        row.getCell(4).setCellStyle(style);
+        Row headRow = sheet.createRow(rowNum++);
+        headRow.createCell(0).setCellValue("Профиль обучения");
+        headRow.getCell(0).setCellStyle(headStyle);
+        headRow.createCell(1).setCellValue("Средний балл");
+        headRow.getCell(1).setCellStyle(headStyle);
+        headRow.createCell(2).setCellValue("Количество студентов");
+        headRow.getCell(2).setCellStyle(headStyle);
+        headRow.createCell(3).setCellValue("Количество университетов");
+        headRow.getCell(3).setCellStyle(headStyle);
+        headRow.createCell(4).setCellValue("Названия университетов");
+        headRow.getCell(4).setCellStyle(headStyle);
+
         for (Statistics statistics : statisticsList) {
-            sheet.createRow(rowNum++);
-            row.createCell(0).setCellValue(statistics.getProfile().getProfileName());
-            row.createCell(1).setCellValue(statistics.getAvgExamScore());
-            row.createCell(2).setCellValue(statistics.getNumberOfStudents());
-            row.createCell(3).setCellValue(statistics.getNumberOfUniversities());
-            row.createCell(4).setCellValue(statistics.getUniversityName());
+            Row dataRow = sheet.createRow(rowNum++);
+            dataRow.createCell(0).setCellValue(statistics.getProfile().getProfileName());
+            dataRow.createCell(1).setCellValue(statistics.getAvgExamScore());
+            dataRow.createCell(2).setCellValue(statistics.getNumberOfStudents());
+            dataRow.createCell(3).setCellValue(statistics.getNumberOfUniversities());
+            dataRow.createCell(4).setCellValue(statistics.getUniversityName());
         }
         try (FileOutputStream fos = new FileOutputStream(filePath)) {
             workbook.write(fos);
